528e63a559cf27b39dcd73a90152c419975ff2c8
Lekhnath Oli <lekhnath@fusemachines.com>
2022-07-28 12:38:52 +0545

    comment api integraion

--- src/consts/emoji.js
+++ src/consts/emoji.js
@@ -1,30 +1,30 @@
 export const emojis = [
   {
-    id: 1,
+    id: 0,
     name: "Very Unhappy",
     value: 1,
     imgUrl: "/assets/images/mood-1.v2.png",
   },
   {
-    id: 2,
+    id: 1,
     name: "Unhappy",
     value: 2,
     imgUrl: "/assets/images/mood-2.v2.png",
   },
   {
-    id: 3,
+    id: 2,
     name: "Neutral",
     value: 3,
     imgUrl: "/assets/images/mood-3.v2.png",
   },
   {
-    id: 4,
+    id: 3,
     name: "Happy",
     value: 4,
     imgUrl: "/assets/images/mood-4.v2.png",
   },
   {
-    id: 5,
+    id: 4,
     name: "Very Happy",
     value: 5,
     imgUrl: "/assets/images/mood-5.v2.png",
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,14 +1,60 @@
 import React, { useEffect } from "react";
-import Emoji from "../../components/Emoji";
-
+import { emojis } from "../../consts/emoji";
+import { API } from "../../utils/api";
 function Home() {
+  const [active, setActive] = React.useState({ activeIndex: -1 });
+  const [name, setName] = React.useState("");
+  const [mood, setMood] = React.useState("");
+  const [comment, setComment] = React.useState("");
+
   useEffect(() => {
     document.body.style.backgroundColor = "#2ce6ce";
-
     return () => {
       document.body.style.backgroundColor = "";
     };
   });
+
+  const handleActive = (i, e) => {
+    setActive({
+      activeIndex: i,
+    });
+    const emojiObj = getName(i);
+    const { name } = emojiObj;
+    setMood(name);
+  };
+
+  const getName = (emojiId) => {
+    const emoji = emojis.find((emoji) => emoji.id === emojiId);
+    return emoji;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const postData = {
+      name,
+      moodValue: mood,
+      comment,
+    };
+    try {
+      const resp = await API.post("/api/comments", postData);
+      const { data } = resp;
+
+      // reset the form
+      setName("");
+      setComment("");
+      setActive({
+        activeIndex: -1,
+      });
+
+      alert(data.message);
+    } catch (e) {
+      if (e.response) {
+        alert(e.response.data.error);
+      }
+      console.log(e);
+    }
+  };
+
   return (
     <div>
       <div className="container">
@@ -24,29 +70,62 @@ function Home() {
         </div>
         <div className="weekly-mood-tracker-box">
           <div className="box-content">
-            <div className="row form-group">
-              <label htmlFor="name" className="text-blue">
-                Name
-              </label>
-              <br />
-              <input type="text" className="form-control" name="name" />
-            </div>
-            <div className="moods form-group">
-              <label htmlFor="mood-options" className="text-blue">
-                How is your week going?
-              </label>
-              <Emoji />
-            </div>
-            <div className="form-group">
-              <label htmlFor="comments" className="text-blue">
-                Comment
-              </label>
-              <br />
-              <textarea className="form-control" name="comment" />
-            </div>
-            <div className="form-group">
-              <button className="btn btn-submit">Submit</button>
-            </div>
+            <form onSubmit={handleSubmit}>
+              <div className="row form-group">
+                <label htmlFor="name" className="text-blue">
+                  Name
+                </label>
+                <br />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="name"
+                  onChange={(e) => setName(e.target.value)}
+                  // required
+                  value={name}
+                />
+              </div>
+              <div className="moods form-group">
+                <label htmlFor="mood-options" className="text-blue">
+                  How is your week going?
+                </label>
+                <div className="mood-options">
+                  {emojis.map((emoji, i) => {
+                    return (
+                      <img
+                        onClick={(e) => handleActive(i, e)}
+                        className={`${
+                          i === active.activeIndex
+                            ? "options bounce-1"
+                            : "options"
+                        }`}
+                        src={emoji.imgUrl}
+                        alt="Emoji"
+                        id={emoji.id}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="form-group">
+                <label htmlFor="comments" className="text-blue">
+                  Comment
+                </label>
+                <br />
+                <textarea
+                  className="form-control"
+                  name="comment"
+                  value={comment}
+                  onChange={(e) => setComment(e.target.value)}
+                />
+              </div>
+              <div className="form-group button-section">
+                <button className="btn btn-submit">Submit</button>
+                <a href="/question">
+                  <span className="text-blue">Questionnaire?</span>
+                </a>
+              </div>
+            </form>
           </div>
         </div>
       </div>
