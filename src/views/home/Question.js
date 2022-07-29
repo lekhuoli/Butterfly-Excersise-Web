56ea802ae9ee69e518bfee37982a8406a35f0060
Lekhnath Oli <lekhnath@fusemachines.com>
2022-07-29 12:18:17 +0545

    adding header to question page

--- src/views/home/Question.js
+++ src/views/home/Question.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Rating from "../../components/Rating";
 
 function Question() {
   return (
-    <>
+    <div className="container">
+      <div className="home-header">
+        <div className="header-row">
+          <div className="logo-text">Butterfly</div>
+          <div className="login">
+            <a href="/dashboard">
+              <button className="btn login-btn">Dashboard</button>
+            </a>
+          </div>
+        </div>
+      </div>
       <Rating />
       <Rating />
       <Rating />
       <Rating />
-    </>
+    </div>
   );
 }
 
